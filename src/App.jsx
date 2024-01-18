bfebe6d34383cfad5258e6755580e7c62e73fa57
unknown <chakcheong@gmail.com>
2024-01-18 21:21:00 +0000

    added landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/NavBar/index'
+import Landing from './components/Landing/index'
 import './App.css'
 // import { library } from '@fortawesome/fontawesome-svg-core'
 // import { fas } from '@fortawesome/free-solid-svg-icons'
@@ -13,6 +14,7 @@ function App() {
   return (
     <Router>
       <div>
+        <Landing />
         <Navbar />
       </div>
     </Router>
--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -9,69 +9,7 @@ import { faNetworkWired } from '@fortawesome/free-solid-svg-icons'
 // Depending on the current path, this component sets the "active" class on the appropriate navigation link item
 function Navbar() {
     return (
-        // <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        //     <div class="container-fluid">
-        //         <Link className="navbar-brand" to="/">
-        //             VisCorner
-        //         </Link>
-        //         <button
-        //             class="navbar-toggler"
-        //             type="button"
-        //             data-bs-toggle="collapse"
-        //             data-bs-target="#navbarNav"
-        //             aria-controls="navbarNav"
-        //             aria-expanded="false"
-        //             aria-label="Toggle navigation"
-        //         >
-        //             <span class="navbar-toggler-icon"></span>
-        //         </button>
-        //         <div className="collapse navbar-collapse" id="navbarNav">
-        //             <ul className="navbar-nav">
-        //                 <li className="nav-item dropdown">
-        //                     <NavLink
-        //                         to="/shipments"
-        //                         end
-        //                         className={({ isActive }) =>
-        //                             isActive ? 'nav-link active dropdown-toggle' : 'nav-link dropdown-toggle'
-        //                         }
-        //                         data-bs-toggle="dropdown"
-        //                     >
-        //                        Shipment
-        //                     </NavLink>
-        //                     <div className="dropdown-menu">
-        //                         {/* Dropdown content goes here */}
-        //                         <a className="dropdown-item" href="#link1">
-        //                         <FontAwesomeIcon icon={faEnvelope} />  Link 1
-        //                         </a>
-        //                         <a className="dropdown-item" href="#link2">
-        //                             Link 2
-        //                         </a>
-        //                     </div>
-        //                 </li>
-        //                 <li className="nav-item">
-        //                     <NavLink
-        //                         to="/users"
-        //                         className={({ isActive }) =>
-        //                             isActive ? 'nav-link active' : 'nav-link'
-        //                         }
-        //                     >
-        //                         Users
-        //                     </NavLink>
-        //                 </li>
-        //                 <li className="nav-item">
-        //                     <NavLink
-        //                         to="/tracking"
-        //                         className={({ isActive }) =>
-        //                             isActive ? 'nav-link active' : 'nav-link'
-        //                         }
-        //                     >
-        //                         Tracking
-        //                     </NavLink>
-        //                 </li>
-        //             </ul>
-        //         </div>
-        //     </div>
-        // </nav>
+
 
         <nav class="navbar bg-body-tertiary fixed-top">
             <div class="container-fluid">
@@ -79,7 +17,7 @@ function Navbar() {
                 <button class="navbar-toggler ms-auto" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
                     <div class="offcanvas-header">
                         <h5 class="offcanvas-title" id="offcanvasNavbarLabel"> <FontAwesomeIcon icon={faNetworkWired} /> VisCore</h5>
                         <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
@@ -113,7 +51,7 @@ function Navbar() {
                                     <NavLink className="dropdown-item" to="#link1">
                                         <FontAwesomeIcon icon={faEnvelope} /> Link 1
                                     </NavLink>
-                                    
+
                                     <NavLink className="dropdown-item" to="#link2">
                                         <FontAwesomeIcon icon={faPen} /> Link 2
                                     </NavLink>
