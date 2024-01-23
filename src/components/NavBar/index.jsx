fd63dcecf2ff11ad453176189f506fa88a3ead59
odunsikazeem <145531096+odunsikazeem@users.noreply.github.com>
2024-01-23 22:56:40 +0000

    Created the request button functionality

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -9,69 +9,6 @@ import { faNetworkWired } from '@fortawesome/free-solid-svg-icons'
 // Depending on the current path, this component sets the "active" class on the appropriate navigation link item
 function NavBar() {
     return (
-        // <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        //     <div class="container-fluid">
-        //         <Link className="navbar-brand" to="/">
-        //             VisCorner
-        //         </Link>
-        //         <button
-        //             class="navbar-toggler"
-        //             type="button"
-        //             data-bs-toggle="collapse"
-        //             data-bs-target="#navbarNav"
-        //             aria-controls="navbarNav"
-        //             aria-expanded="false"
-        //             aria-label="Toggle navigation"
-        //         >
-        //             <span class="navbar-toggler-icon"></span>
-        //         </button>
-        //         <div className="collapse navbar-collapse" id="navbarNav">
-        //             <ul className="navbar-nav">
-        //                 <li className="nav-item dropdown">
-        //                     <NavLink
-        //                         to="/shipments"
-        //                         end
-        //                         className={({ isActive }) =>
-        //                             isActive ? 'nav-link active dropdown-toggle' : 'nav-link dropdown-toggle'
-        //                         }
-        //                         data-bs-toggle="dropdown"
-        //                     >
-        //                        Shipment
-        //                     </NavLink>
-        //                     <div className="dropdown-menu">
-        //                         {/* Dropdown content goes here */}
-        //                         <a className="dropdown-item" href="#link1">
-        //                         <FontAwesomeIcon icon={faEnvelope} />  Link 1
-        //                         </a>
-        //                         <a className="dropdown-item" href="#link2">
-        //                             Link 2
-        //                         </a>
-        //                     </div>
-        //                 </li>
-        //                 <li className="nav-item">
-        //                     <NavLink
-        //                         to="/users"
-        //                         className={({ isActive }) =>
-        //                             isActive ? 'nav-link active' : 'nav-link'
-        //                         }
-        //                     >
-        //                         Users
-        //                     </NavLink>
-        //                 </li>
-        //                 <li className="nav-item">
-        //                     <NavLink
-        //                         to="/tracking"
-        //                         className={({ isActive }) =>
-        //                             isActive ? 'nav-link active' : 'nav-link'
-        //                         }
-        //                     >
-        //                         Tracking
-        //                     </NavLink>
-        //                 </li>
-        //             </ul>
-        //         </div>
-        //     </div>
-        // </nav>
 
         <nav class="navbar bg-body-tertiary fixed-top">
             <div class="container-fluid">
@@ -88,7 +25,7 @@ function NavBar() {
                         <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
                             <li class="nav-item">
                                 <NavLink
-                                    to="/about_us"
+                                    to="home"
                                     end
                                     className={({ isActive }) =>
                                         isActive ? 'nav-link active' : 'nav-link'
@@ -97,54 +34,19 @@ function NavBar() {
                                     About Us
                                 </NavLink>
                             </li>
-                            <li className="nav-item dropdown">
+                            <li class="nav-item">
                                 <NavLink
-                                    to="/shipments"
+                                    to="request"
                                     end
                                     className={({ isActive }) =>
-                                        isActive ? 'nav-link active dropdown-toggle' : 'nav-link dropdown-toggle'
+                                        isActive ? 'nav-link active' : 'nav-link'
                                     }
-                                    data-bs-toggle="dropdown"
                                 >
-                                    Shipment
+                                    Request
                                 </NavLink>
-                                <div className="dropdown-menu">
-                                    {/* Dropdown content goes here */}
-                                    <NavLink className="dropdown-item" to="#link1">
-                                        <FontAwesomeIcon icon={faEnvelope} /> Link 1
-                                    </NavLink>
-                                    
-                                    <NavLink className="dropdown-item" to="#link2">
-                                        <FontAwesomeIcon icon={faPen} /> Link 2
-                                    </NavLink>
-                                </div>
                             </li>
-                            <li className="nav-item dropdown">
-                                <NavLink
-                                    to="/shipments"
-                                    end
-                                    className={({ isActive }) =>
-                                        isActive ? 'nav-link active dropdown-toggle' : 'nav-link dropdown-toggle'
-                                    }
-                                    data-bs-toggle="dropdown"
-                                >
-                                    Something Else
-                                </NavLink>
-                                <div className="dropdown-menu">
-                                    {/* Dropdown content goes here */}
-                                    <a className="dropdown-item" href="#link1">
-                                        <FontAwesomeIcon icon={faEnvelope} />  Link 1
-                                    </a>
-                                    <a className="dropdown-item" href="#link2">
-                                        <FontAwesomeIcon icon={faPen} /> Link 2
-                                    </a>
-                                </div>
-                            </li>
+                            
                         </ul>
-                        <form class="d-flex mt-3" role="search">
-                            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"></input>
-                            <button class="btn btn-outline-success" type="submit">Search</button>
-                        </form>
                     </div>
                 </div>
             </div>
--- src/components/pages/Dashboard.jsx
+++ src/components/pages/Dashboard.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import NavBar from '../NavBar/index';
 import Footer from '../Footer';
 import CustomerRequests from '../Card';
+import Landing from '../Landing';
 
 function Dashboard() {
   return (
 
       <div>
         <NavBar />
-        <CustomerRequests />
+        {/* <CustomerRequests /> */}
+        <Landing />
         <Footer />
       </div>
 
